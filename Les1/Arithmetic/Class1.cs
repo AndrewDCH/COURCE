929e6f749342ae4216748d129f55002f76e7e2e7
AndrewDCH <andry13net@gmail.com>
2019-06-25 12:43:15 +0300

    Les1

---
+++ Les1/Arithmetic/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Arithmetic
+{
+    public class Arithmitic
+    {
+        public static int Add(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+        public static int Subtract(int value1, int value2)
+        {
+            return value1 - value2;
+        }
+        public static int Multiply(int value1, int value2)
+        {
+            return value1 * value2;
+        }
+        public static int Divide(int value1, int value2)
+        {
+            return value1 / value2;
+        }
+    }
+}
---
+++ Les1/ConsolPrj3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arithmetic;
+
+namespace ConsolPrj3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 6;
+            Console.WriteLine(Arithmitic.Add(a, b));
+            Console.WriteLine(Arithmitic.Subtract(a, b));
+            Console.WriteLine(Arithmitic.Multiply(a, b));
+            Console.WriteLine(Arithmitic.Divide(a, b));
+            Console.ReadKey();
+        }
+    }
+}
