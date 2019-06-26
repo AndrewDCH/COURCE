9d16781948f75bc8d3741ecc7cc30aa7bb9ba14c
AndrewDCH <andry13net@gmail.com>
2019-06-26 14:25:20 +0300

    Les4

---
+++ Les4/Les4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Square
+    {
+        private double _sideOfSquare, _perimeter, _square;
+        public Square()
+        {
+            _sideOfSquare = 0;
+        }
+        public Square(double sideOfSquare)
+        {
+            this._sideOfSquare = sideOfSquare;
+        }
+        public double PerimeterCalculation()
+        {
+            return 4 * _sideOfSquare;
+        }
+        public double SquareCalculation()
+        {
+            return _sideOfSquare * _sideOfSquare;
+        }
+    };
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square k = new Square(3);
+            Console.WriteLine("square={0}", k.SquareCalculation());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Les4/Task2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class ArithmeticProgression
+    {
+        private int _firstMemberOfProgress, _stepOfProgression;
+        public int FirstMember
+        {
+            get
+            {
+                return this._firstMemberOfProgress;
+            }
+            set
+            {
+                _firstMemberOfProgress = value;
+            }
+        }
+        public int StepOfProgression
+        {
+            get
+            {
+                return this._stepOfProgression;
+            }
+            set
+            {
+                _stepOfProgression = value;
+            }
+        }
+        public ArithmeticProgression()
+        {
+            _firstMemberOfProgress = 0;
+            _stepOfProgression = 0;
+        }
+        public ArithmeticProgression(int firstMemberOfProgression)
+        {
+            _firstMemberOfProgress = firstMemberOfProgression;
+        }
+        public ArithmeticProgression(int firstMemberOfProgression, int stepOfProgression)
+        {
+            _firstMemberOfProgress = firstMemberOfProgression;
+            _stepOfProgression = stepOfProgression;
+        }
+        public double AvgOfSumOfProgression(int numberOfMembers)
+        {
+            int sum=0, k=_firstMemberOfProgress;
+            for(int i=0;i<numberOfMembers;i++){
+                sum += k;
+                k += _stepOfProgression;
+            }
+            return sum / numberOfMembers;
+        }
+        public int SumOfNMembers(int nMember)
+        {
+            int sum = 0 ,k=_firstMemberOfProgress;
+            for(int i = 0; i < nMember; i ++)
+            {
+
+                sum += k;
+                k += _stepOfProgression;
+            }
+            return sum;
+        }
+
+    };
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithmeticProgression a = new ArithmeticProgression(2, 3);
+            Console.WriteLine(a.SumOfNMembers(5));
+            Console.ReadKey();
+        }
+
+    }
+}
